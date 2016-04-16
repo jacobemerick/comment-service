672b8107025a0f6428e48a6a7411158ca2f2b950
jacobemerick <jpemeric@gmail.com>
2016-04-16 13:44:39 -0700

    Adds Aura/Di as prototype container-interop usage

--- public/index.php
+++ public/index.php
@@ -2,10 +2,15 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
+use Aura\Di\ContainerBuilder;
 use Jacobemerick\Talus\Talus;
 
+$builder = new ContainerBuilder();
+$di = $builder->newInstance();
 $swagger = fopen('../swagger.json', 'r');
+
 $talus = new Talus([
+    'container' => $di,
     'swagger' => $swagger,
 ]);
 $talus->run();
