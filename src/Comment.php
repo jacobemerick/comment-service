360b9f7e0cec242c19fddaeee611365b5c680fa3
jacobemerick <jpemeric@gmail.com>
2016-04-28 18:43:41 -0700

    Breaks out parameters, adds more flair to swagger

---
+++ src/Comment.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Jacobemerick\CommentService;
+
+use Interop\Container\ContainerInterface as Container;
+use Psr\Http\Message\RequestInterface as Request;
+use Psr\Http\Message\ResponseInterface as Response;
+
+class Comment
+{
+
+    /** @var Container */
+    protected $container;
+
+    /**
+     * @param Container $container
+     */
+    public function __construct(Container $container)
+    {
+        $this->container = $container;
+    }
+
+    /**
+     * @param Request $request
+     * @param Response $response
+     */
+    public function getComments(Request $request, Response $response)
+    {
+        echo 'yay get comments called';
+    }
+
+    /**
+     * @param Request $request
+     * @param Response $response
+     */
+    public function createComment(Request $request, Response $response)
+    {
+        // todo something something validation
+        // todo something something data layers
+
+        $query = "
+            SELECT `id`
+            FROM `commenter`
+            WHERE `name` = :name AND
+                  `email` = :email AND
+                  `url` = :url
+            LIMIT 1";
+        $params = [
+            'name' => $request->getParsedBody()['commenter']['name'],
+            'email' => $request->getParsedBody()['commenter']['email'],
+            'url' => $request->getParsedBody()['commenter']['url'],
+        ];
+        var_dump($params);
+        return;
+        $commenter = $this->container->db->selectOne($query, $params);
+    }
+}
