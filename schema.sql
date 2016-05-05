e26d250dabc1075378044f62966b32313e27a155
jacobemerick <jpemeric@gmail.com>
2016-05-04 20:31:01 -0700

    Adds db schema

---
+++ schema.sql
@@ -0,0 +1,144 @@
+# ************************************************************
+# Sequel Pro SQL dump
+# Version 4499
+#
+# http://www.sequelpro.com/
+# https://github.com/sequelpro/sequelpro
+# ************************************************************
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+# Dump of table comment
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `comment`;
+
+CREATE TABLE `comment` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `commenter` int(11) unsigned NOT NULL DEFAULT '0',
+  `comment_body` int(11) unsigned NOT NULL DEFAULT '0',
+  `comment_location` int(11) unsigned NOT NULL DEFAULT '0',
+  `comment_request` int(11) DEFAULT '0',
+  `notify` tinyint(1) NOT NULL DEFAULT '0',
+  `display` tinyint(1) NOT NULL DEFAULT '0',
+  `create_time` datetime NOT NULL,
+  `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+# Dump of table comment_body
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `comment_body`;
+
+CREATE TABLE `comment_body` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `body` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+# Dump of table comment_domain
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `comment_domain`;
+
+CREATE TABLE `comment_domain` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `domain` varchar(100) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+# Dump of table comment_location
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `comment_location`;
+
+CREATE TABLE `comment_location` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `domain` int(11) unsigned NOT NULL,
+  `page` int(11) unsigned DEFAULT NULL,
+  `thread` int(11) unsigned DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+# Dump of table comment_page
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `comment_page`;
+
+CREATE TABLE `comment_page` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `page` varchar(100) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+# Dump of table comment_request
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `comment_request`;
+
+CREATE TABLE `comment_request` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `ip_address` binary(16) NOT NULL DEFAULT '\\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0',
+  `user_agent` text NOT NULL,
+  `referrer` text,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+# Dump of table comment_thread
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `comment_thread`;
+
+CREATE TABLE `comment_thread` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `thread` varchar(100) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+# Dump of table commenter
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `commenter`;
+
+CREATE TABLE `commenter` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL DEFAULT '',
+  `email` varchar(100) NOT NULL DEFAULT '',
+  `url` varchar(100) DEFAULT NULL,
+  `key` char(10) NOT NULL DEFAULT '',
+  `is_trusted` tinyint(1) unsigned NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
