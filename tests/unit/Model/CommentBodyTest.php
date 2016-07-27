1a63c6bcbfb3ba4475986344bc6a8914e2a3474b
jacobemerick <jpemeric@gmail.com>
2016-07-27 07:24:47 -0600

    Adds unit test around commentbody model as prototype

---
+++ tests/unit/Model/CommentBodyTest.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Jacobemerick\CommentService\Model;
+
+use Aura\Sql\ExtendedPdo;
+use PHPUnit_Framework_TestCase;
+
+class CommentBodyTest extends PHPUnit_Framework_TestCase
+{
+
+    public function testIsInstanceOfCommentBody()
+    {
+        $mockPdo = $this->createMock(ExtendedPdo::class);
+        $model = new CommentBody($mockPdo);
+
+        $this->assertInstanceOf(CommentBody::class, $model);
+    }
+
+    public function testConstructSetsExtendedPdo()
+    {
+        $mockPdo = $this->createMock(ExtendedPdo::class);
+        $model = new CommentBody($mockPdo);
+
+        $this->assertAttributeSame($mockPdo, 'extendedPdo', $model);
+    }
+
+    public function testCreateMethodSendsParams()
+    {
+        $query = "
+            INSERT INTO
+                `comment_body` (`body`)
+            VALUES
+                (:body)";
+        $body = 'this is a comment';
+
+        $mockPdo = $this->createMock(ExtendedPdo::class);
+        $mockPdo->method('perform')
+            ->with(
+                $this->equalTo($query),
+                $this->equalTo([
+                    'body' => $body,
+                ])
+            )
+            ->willReturn(true);
+
+        $model = new CommentBody($mockPdo);
+        $model->create($body);
+    }
+
+    public function testCreateMethodReturnsInsertId()
+    {
+        $lastInsertId = 123;
+
+        $mockPdo = $this->createMock(ExtendedPdo::class);
+        $mockPdo->method('lastInsertId')
+            ->willReturn($lastInsertId);
+
+        $model = new CommentBody($mockPdo);
+        $result = $model->create('this is a comment');
+
+        $this->assertSame($lastInsertId, $result);
+    }
+}
