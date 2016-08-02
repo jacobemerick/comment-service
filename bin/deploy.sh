ea419e6ea883f847c91ac4ccf82ff0f4da831428
jacobemerick <jpemeric@gmail.com>
2016-08-02 16:16:56 -0700

    Bash scripts are haaaaard

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -2,26 +2,28 @@
 
 if [ "${TRAVIS_PULL_REQUEST}" == "false" ] && [ "${TRAVIS_BRANCH}" == "master" ]; then
 
-  echo "Clears git information\n"
+  echo "Clears git information"
   rm -rf .git
 
-  echo "Writing custom gitignore for build\n"
-  cat "# Build Ignores\n" > .gitignore
-  cat "composer.phar\n" >> .gitignore
-  cat "config.json\n" >> .gitignore
-  cat "deploy_key.*\n" >> .gitignore
-  cat "codeclimate.json\n" >> .gitignore
-  cat "coverage.xml\n" >> .gitignore
+  echo "Writing custom gitignore for build"
+  echo "# Build Ignores" > .gitignore
+  echo "composer.phar" >> .gitignore
+  echo "config.json" >> .gitignore
+  echo "deploy_key.*" >> .gitignore
+  echo "codeclimate.json" >> .gitignore
+  echo "coverage.xml" >> .gitignore
 
-  echo "Sets up package for sending\n"
+  echo "Sets up package for sending"
   git init
   git remote add deploy $DEPLOY_URI
-  git add -f -all .
+  git config user.name $DEPLOY_USER
+  git config user.email $DEPLOY_EMAIL
+  git add --all .
   git commit -m "Deply from Travis - build {$TRAVIS_BUILD_NUMBER}"
 
-  echo "Sets up permissions\n"
-  echo -e "Host comments.reynrick.com\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
-  openssl aes-256-cbc -K $encrypted_a9d53792e855_key -iv $encrypted_a9d53792e855_iv in deploy_key.pem.enc -out deploy_key.pem -d
+  echo "Sets up permissions"
+  echo -e "Host comments.reynrick.com\n\tStrictHostKeyChecking no" >> ~/.ssh/config
+  openssl aes-256-cbc -K $encrypted_a9d53792e855_key -iv $encrypted_a9d53792e855_iv -in deploy_key.pem.enc -out deploy_key.pem -d
   eval "$(ssh-agent -s)"
   chmod 600 deploy_key.pem
   ssh-add deploy_key.pem
