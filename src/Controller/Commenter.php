73d144b9d5f288b1e9844f7bbf30b88fc34c2a86
jacobemerick <jpemeric@gmail.com>
2016-08-01 19:31:16 -0700

    Be more obvious about the passed in request interface

--- src/Controller/Commenter.php
+++ src/Controller/Commenter.php
@@ -5,7 +5,7 @@ namespace Jacobemerick\CommentService\Controller;
 use Interop\Container\ContainerInterface as Container;
 use Jacobemerick\CommentService\Model\Commenter as CommenterModel;
 use Jacobemerick\CommentService\Serializer\Commenter as CommenterSerializer;
-use Psr\Http\Message\RequestInterface as Request;
+use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
 
 class Commenter
