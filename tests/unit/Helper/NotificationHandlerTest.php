ffca3b8530f840b4be8bf21beee7268821c175f8
jacobemerick <jpemeric@gmail.com>
2016-08-22 05:30:13 -0700

    Adds tests around protected helper methods

--- tests/unit/Helper/NotificationHandlerTest.php
+++ tests/unit/Helper/NotificationHandlerTest.php
@@ -3,8 +3,10 @@
 namespace Jacobemerick\CommentService\Helper;
 
 use Aura\Sql\ExtendedPdo;
+use DateTime;
 use Jacobemerick\Archangel\Archangel;
 use PHPUnit_Framework_TestCase;
+use ReflectionClass;
 
 class NotificationHandlerTest extends PHPUnit_Framework_TestCase
 {
@@ -26,5 +28,144 @@ class NotificationHandlerTest extends PHPUnit_Framework_TestCase
 
         $this->assertAttributeSame($mockPdo, 'dbal', $notificationHandler);
         $this->assertAttributeSame($mockArchangel, 'mailer', $notificationHandler);
+    }
+
+    // __invoke tests
+
+    public function testGetTemplateParametersForBlog()
+    {
+        $mockPdo = $this->createMock(ExtendedPdo::class);
+        $mockArchangel = $this->createMock(Archangel::class);
+        $notificationHandler = new NotificationHandler($mockPdo, $mockArchangel);
+
+        $reflectedGetTemplateParameters = (new ReflectionClass($notificationHandler))
+            ->getMethod('getTemplateParameters');
+        $reflectedGetTemplateParameters->setAccessible(true);
+
+        $templateParameters = $reflectedGetTemplateParameters->invokeArgs(
+            $notificationHandler,
+            [ 'blog.jacobemerick.com' ]
+        );
+
+        $this->assertEquals(
+            [
+                'pageType' => 'post',
+                'domainTitle' => "Jacob Emerick's Blog",
+            ],
+            $templateParameters
+        );
+    }
+
+    public function testGetTemplateParametersForWaterfalls()
+    {
+        $mockPdo = $this->createMock(ExtendedPdo::class);
+        $mockArchangel = $this->createMock(Archangel::class);
+        $notificationHandler = new NotificationHandler($mockPdo, $mockArchangel);
+
+        $reflectedGetTemplateParameters = (new ReflectionClass($notificationHandler))
+            ->getMethod('getTemplateParameters');
+        $reflectedGetTemplateParameters->setAccessible(true);
+
+        $templateParameters = $reflectedGetTemplateParameters->invokeArgs(
+            $notificationHandler,
+            [ 'waterfallsofthekeweenaw.com' ]
+        );
+
+        $this->assertEquals(
+            [
+                'pageType' => 'page',
+                'domainTitle' => 'Waterfalls of the Keweenaw',
+            ],
+            $templateParameters
+        );
+    }
+
+    public function testGetTemplateParametersForDefault()
+    {
+        $undefinedDomain = 'domain.tld';
+
+        $mockPdo = $this->createMock(ExtendedPdo::class);
+        $mockArchangel = $this->createMock(Archangel::class);
+        $notificationHandler = new NotificationHandler($mockPdo, $mockArchangel);
+
+        $reflectedGetTemplateParameters = (new ReflectionClass($notificationHandler))
+            ->getMethod('getTemplateParameters');
+        $reflectedGetTemplateParameters->setAccessible(true);
+
+        $templateParameters = $reflectedGetTemplateParameters->invokeArgs(
+            $notificationHandler,
+            [ $undefinedDomain ]
+        );
+
+        $this->assertEquals(
+            [
+                'pageType' => 'page',
+                'domainTitle' => $undefinedDomain,
+            ],
+            $templateParameters
+        );
+    }
+
+    public function testGetSubject()
+    {
+        $domainTitle = 'Waterfalls of the Keweenaw';
+
+        $mockPdo = $this->createMock(ExtendedPdo::class);
+        $mockArchangel = $this->createMock(Archangel::class);
+        $notificationHandler = new NotificationHandler($mockPdo, $mockArchangel);
+
+        $reflectedNotificationHandler = new ReflectionClass($notificationHandler);
+        $reflectedSubjectTemplate = $reflectedNotificationHandler->getProperty('subjectTemplate');
+        $reflectedSubjectTemplate->setAccessible(true);
+        $reflectedGetSubject = $reflectedNotificationHandler->getMethod('getSubject');
+        $reflectedGetSubject->setAccessible(true);
+
+        $subjectTemplate = $reflectedSubjectTemplate->getValue($notificationHandler);
+        $expectedSubject = sprintf($subjectTemplate, $domainTitle);
+
+        $subject = $reflectedGetSubject->invokeArgs($notificationHandler, [ $domainTitle ]);
+
+        $this->assertEquals($expectedSubject, $subject);
+    }
+
+    public function testGetMessage()
+    {
+        $pageType = 'post';
+        $domainTitle = 'some blog';
+        $commentDate = new DateTime('4:13 pm August 21, 2015');
+        $commenterName = 'Jack Black';
+        $comment = 'this is a comment';
+        $commentUrl = 'blog.tld/path';
+ 
+        $mockPdo = $this->createMock(ExtendedPdo::class);
+        $mockArchangel = $this->createMock(Archangel::class);
+        $notificationHandler = new NotificationHandler($mockPdo, $mockArchangel);
+
+        $reflectedNotificationHandler = new ReflectionClass($notificationHandler);
+        $reflectedMessageTemplate = $reflectedNotificationHandler->getProperty('messageTemplate');
+        $reflectedMessageTemplate->setAccessible(true);
+        $reflectedGetMessage = $reflectedNotificationHandler->getMethod('getMessage');
+        $reflectedGetMessage->setAccessible(true);
+
+        $messageTemplate = $reflectedMessageTemplate->getValue($notificationHandler);
+        $expectedMessage = sprintf($messageTemplate,
+            $pageType,
+            $domainTitle,
+            $commentDate->format('F j, Y g:i a'),
+            $commenterName,
+            $comment,
+            $commentUrl
+        );
+
+        $message = $reflectedGetMessage->invokeArgs($notificationHandler, [
+            $pageType,
+            $domainTitle,
+            $commentDate,
+            $commenterName,
+            $comment,
+            $commentUrl
+        ]);
+
+        $this->assertEquals($expectedMessage, $message);
     }
 }
