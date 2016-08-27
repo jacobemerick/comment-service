074650a00a8f329efd6af3126978b6097866f555
jacobemerick <jpemeric@gmail.com>
2016-08-26 18:00:25 -0700

    Adds authentication middleware unit tests

--- tests/unit/Middleware/AuthenticationTest.php
+++ tests/unit/Middleware/AuthenticationTest.php
@@ -2,7 +2,12 @@
 
 namespace Jacobemerick\CommentService\Middleware;
 
+use Exception;
 use PHPUnit_Framework_TestCase;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Message\UriInterface as Uri;
+use ReflectionClass;
 
 class AuthenticationTest extends PHPUnit_Framework_TestCase
 {
@@ -25,21 +30,140 @@ class AuthenticationTest extends PHPUnit_Framework_TestCase
 
     public function testInvokeSkipsApiDocsRoute()
     {
-        $this->markTestIncomplete('todo');
+        $mockUri = $this->createMock(Uri::class);
+        $mockUri->method('getPath')
+            ->willReturn('/api-docs');
+
+        $mockRequest = $this->createMock(Request::class);
+        $mockRequest->method('getUri')
+            ->willReturn($mockUri);
+        $mockRequest->expects($this->never())
+            ->method('getHeader');
+
+        $mockResponseReturn = $this->createMock(Response::class);
+
+        $mockResponse = $this->createMock(Response::class);
+
+        $callable = function ($req, $res) use ($mockResponseReturn) {
+            return $mockResponseReturn;
+        };
+
+        $response = (new Authentication('', ''))($mockRequest, $mockResponse, $callable);
+
+        $this->assertSame($mockResponseReturn, $response);
+    }
+
+    public function testInvokeReturns403ForNoCredentials()
+    {
+        $mockUri = $this->createMock(Uri::class);
+        $mockUri->method('getPath')
+            ->willReturn('/path');
+
+        $mockRequest = $this->createMock(Request::class);
+        $mockRequest->method('getUri')
+            ->willReturn($mockUri);
+        $mockRequest->method('getHeader')
+            ->willReturn([]);
+
+        $mockResponseReturn = $this->createMock(Response::class);
+
+        $mockResponse = $this->createMock(Response::class);
+        $mockResponse->expects($this->once())
+            ->method('withStatus')
+            ->with(
+                $this->equalTo(403)
+            )
+            ->willReturn($mockResponseReturn);
+
+        $callable = function ($req, $res) {
+            throw new Exception('callable was called');
+        };
+
+        $response = (new Authentication('', ''))($mockRequest, $mockResponse, $callable);
+
+        $this->assertSame($mockResponseReturn, $response);
     }
 
     public function testInvokeReturns403ForInvalidCredentials()
     {
-        $this->markTestIncomplete('todo');
+        $mockUri = $this->createMock(Uri::class);
+        $mockUri->method('getPath')
+            ->willReturn('/path');
+
+        $mockRequest = $this->createMock(Request::class);
+        $mockRequest->method('getUri')
+            ->willReturn($mockUri);
+        $mockRequest->method('getHeader')
+            ->willReturn([ 'Invalid Auth' ]);
+
+        $mockResponseReturn = $this->createMock(Response::class);
+
+        $mockResponse = $this->createMock(Response::class);
+        $mockResponse->expects($this->once())
+            ->method('withStatus')
+            ->with(
+                $this->equalTo(403)
+            )
+            ->willReturn($mockResponseReturn);
+
+        $callable = function ($req, $res) {
+            return $mockResponseReturn;
+        };
+
+        $response = (new Authentication('', ''))($mockRequest, $mockResponse, $callable);
+
+        $this->assertSame($mockResponseReturn, $response);
     }
 
     public function testInvokeReturnsCallbackForValidCredentials()
     {
-        $this->markTestIncomplete('todo');
+        $username = 'test-user';
+        $password = 'test-pass';
+
+        $authHeader = base64_encode("{$username}:{$password}");
+        $authHeader = "Basic {$authHeader}";
+
+        $mockUri = $this->createMock(Uri::class);
+        $mockUri->method('getPath')
+            ->willReturn('/path');
+
+        $mockRequest = $this->createMock(Request::class);
+        $mockRequest->method('getUri')
+            ->willReturn($mockUri);
+        $mockRequest->method('getHeader')
+            ->willReturn([ $authHeader ]);
+
+        $mockResponseReturn = $this->createMock(Response::class);
+
+        $mockResponse = $this->createMock(Response::class);
+        $mockResponse->expects($this->never())
+            ->method('withStatus');
+
+        $callable = function ($req, $res) use ($mockResponseReturn) {
+            return $mockResponseReturn;
+        };
+
+        $response = (new Authentication($username, $password))($mockRequest, $mockResponse, $callable);
+
+        $this->assertSame($mockResponseReturn, $response);
     }
 
     public function testGetAuthHeaderReturnsEncodedHeader()
     {
-        $this->markTestIncomplete('todo');
+        $username = 'test-user';
+        $password = 'test-pass';
+
+        $expectedAuthHeader = base64_encode("{$username}:{$password}");
+        $expectedAuthHeader = "Basic {$expectedAuthHeader}";
+
+        $authentication = new Authentication($username, $password);
+
+        $reflectedGetAuthHeader = (new ReflectionClass($authentication))
+            ->getMethod('getAuthHeader');
+        $reflectedGetAuthHeader->setAccessible(true);
+
+        $authHeader = $reflectedGetAuthHeader->invoke($authentication);
+
+        $this->assertEquals($expectedAuthHeader, $authHeader);
     }
 }
