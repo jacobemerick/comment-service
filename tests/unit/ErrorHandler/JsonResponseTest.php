bd285518657d3bf40575f7acd1e46fa6f6df10cb
jacobemerick <jpemeric@gmail.com>
2016-08-26 17:18:27 -0700

    Adds test around error handler

--- tests/unit/ErrorHandler/JsonResponseTest.php
+++ tests/unit/ErrorHandler/JsonResponseTest.php
@@ -2,7 +2,11 @@
 
 namespace Jacobemerick\CommentService\ErrorHandler;
 
+use Exception;
 use PHPUnit_Framework_TestCase;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Message\StreamInterface as Stream;
 
 class JsonResponseTest extends PHPUnit_Framework_TestCase
 {
@@ -15,11 +19,37 @@ class JsonResponseTest extends PHPUnit_Framework_TestCase
 
     public function testInvokeWritesEncodedErrorToResponse()
     {
-        $this->markTestIncomplete('todo');
+        $errorMessage = 'Test Error Message';
+
+        $mockRequest = $this->createMock(Request::class);
+
+        $mockStream = $this->createMock(Stream::class);
+        $mockStream->expects($this->once())
+            ->method('write')
+            ->with(
+                $this->equalTo(json_encode([ 'error' => $errorMessage ]))
+            );
+
+        $mockResponse = $this->createMock(Response::class);
+        $mockResponse->method('getBody')
+            ->willReturn($mockStream);
+
+        $exception = new Exception($errorMessage);
+
+        (new JsonResponse())($mockRequest, $mockResponse, $exception);
     }
 
     public function testInvokeReturnsResponse()
     {
-        $this->markTestIncomplete('todo');
+        $mockRequest = $this->createMock(Request::class);
+
+        $mockResponse = $this->createMock(Response::class);
+        $mockResponse->method('getBody')
+            ->willReturn($this->createMock(Stream::class));
+
+        $exception = new Exception();
+
+        $response = (new JsonResponse())($mockRequest, $mockResponse, $exception);
+        $this->assertSame($mockResponse, $response);
     }
 }
